aaa8acd2affc97bb536e026ed19fa005002d937c
eduaardofranco <eduaardo.franco@gmail.com>
2023-09-08 19:47:05 +0100

    add middleware authentication

---
+++ src/configs/upload.js
@@ -0,0 +1,7 @@
+const path = require('path')
+const multer = require("multer")
+
+
+const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp")
+const UPLOADS_FOLDER = path.resolve(__dirname, "uploads")
+
